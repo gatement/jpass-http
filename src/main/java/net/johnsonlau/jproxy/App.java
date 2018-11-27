efe45c4cdd3fbcf37f6e2b5731c9a9e0b4615e5f
liugh22 <guanhua.liu@midea.com>
2018-11-27 17:18:22 +0800

    go

--- src/main/java/net/johnsonlau/jproxy/App.java
+++ src/main/java/net/johnsonlau/jproxy/App.java
@@ -1,7 +1,14 @@
 package net.johnsonlau.jproxy;
 
 public class App {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.println("Hello World!");
+
+		int port = 8119;
+		if (args.length > 0) {
+			port = Integer.parseInt(args[0]);
+		}
+
+		new DiscardServer(port).run();
 	}
 }
