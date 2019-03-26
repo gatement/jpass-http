662c38ad92f70d9f561cecee934cfc4411a693ff
JohnsonLau <gatement@gmail.com>
2019-03-26 21:52:54 +0800

    go

--- src/main/java/net/johnsonlau/jproxy/conf/MyProxySettings.java
+++ src/main/java/net/johnsonlau/jproxy/conf/MyProxySettings.java
@@ -6,21 +6,26 @@ public class MyProxySettings extends ProxySettings {
 
 	@Override
 	public String getSshHost() {
-		return "lqahk";
+		return "1.2.3.4";
 	}
 
 	@Override
 	public int getSshPort() {
-		return 9812;
+		return 22;
 	}
 
 	@Override
 	public String getSshUser() {
-		return "johnson";
+		return "root";
 	}
 
 	@Override
 	public String getSshPwd() {
 		return "123456";
+	}
+	
+	@Override
+	public int getProxyPort() {
+		return 8119;
 	}
 }
