e88793558f6927104aeac9a7317f72ee14927163
liugh22 <guanhua.liu@midea.com>
2019-06-03 08:39:28 +0800

    go

--- src/main/java/net/johnsonlau/jproxy/App.java
+++ src/main/java/net/johnsonlau/jproxy/App.java
@@ -7,12 +7,12 @@ import net.johnsonlau.jproxy.lib.conf.ProxySettings;
 public class App {
 	public static void main(String[] args) {
 		ProxySettings settings = new ProxySettings();
-		settings.setServerAddr("");
-		settings.setServerPort(22);
-		settings.setUsername("root");
-		settings.setPassword("");
-		settings.setProxyPort(8119);
-
+		settings.setServerAddr(System.getProperty("serverAddr", ""));
+		settings.setServerPort(Integer.parseInt(System.getProperty("serverPort", "22")));
+		settings.setUsername(System.getProperty("username", "root"));
+		settings.setPassword(System.getProperty("password", ""));
+		settings.setProxyPort(Integer.parseInt(System.getProperty("serverPort", "8119")));
+		
 		new ProxyMain(settings, new MyProxyLog()).run();
 	}
 }
